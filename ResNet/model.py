a86b74d4b940707494672d4ce608f24639330ba3
mango1698 <mango_1698@163.com>
2024-01-16 15:41:04 +0800

    ResNet

--- ResNet/model.py
+++ ResNet/model.py
@@ -2,60 +2,145 @@
 @Author  :shw
 @Date    :2024/1/16 11:24
 """
+import torch
 from torch import nn
 from torch.nn import functional as F
 
+# resnet18 resnet34 block
+class BasicBlock(nn.Module):
+    expansion = 1
 
-cfgs = {
-    'resnet18':[2,2,2,2],
-    'resnet34':[3,4,6,3],
-    'resnet50':[3,4,6,3],
-    'resnet101':[3,4,23,3],
-    'resnet152':[3,8,36,3]
-}
-
-class ShortCut(nn.Module):
-
-    def __init__(self,in_channel,out_channel,num,kernel_size,fout, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.num = num
-        if num==2:
-            self.conv1 = nn.Conv2d(in_channel,out_channel,kernel_size)
-            self.bn1 = nn.BatchNorm2d(out_channel)
-            self.conv2 = nn.Conv2d(out_channel,out_channel,kernel_size)
-            self.bn2 = nn.BatchNorm2d(out_channel)
-        elif num==3:
-            self.conv1 = nn.Conv2d(in_channel,out_channel,1)
-            self.bn1 = nn.BatchNorm2d(out_channel)
-            self.conv2 = nn.Conv2d(out_channel,out_channel,kernel_size)
-            self.bn2 = nn.BatchNorm2d(out_channel)
-            self.conv3 = nn.Conv2d(out_channel,fout,1)
-            self.bn3 = nn.BatchNorm2d(fout)
-        if num==2:
-            self.conv1x1 = nn.Conv2d(in_channel,out_channel,kernel_size=1)
-        elif num==3:
-            self.conv1x1 = nn.Conv2d(in_channel,fout,kernel_size=1)
+    def __init__(self,in_channels,out_channels,stride=1,downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels,out_channels,kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels,out_channels,kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
 
     def forward(self, x):
-        if self.num==2:
-            x1 = self.conv1(x)
-            x1 = F.relu(self.bn1(x1))
-            x1 = self.conv2(x1)
-            x1 = self.bn2(x1)
-            x2 = self.conv1x1(x)
-            import torch
-            x = torch.cat([x1,x2],dim=1)
-            x = F.relu(x)
-        elif self.num==3:
-            x1 = self.conv1(x)
-            x1 = F.relu(self.bn1(x1))
-            x1 = self.conv2(x1)
-            x1 = F.relu(self.bn2(x1))
-            x1 = self.conv3(x1)
-            x1 = self.bn3(x1)
-            x2 = self.conv1x1(x)
-            import torch
-            x = torch.cat([x1, x2], dim=1)
-            x = F.relu(x)
+        identity = x
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        out = self.conv1(x)
+        out = F.relu(self.bn1(out), inplace=True)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        out += identity
+        out = F.relu(out, inplace=True)
+        return out
+
+# resnet50 101 152 block
+class Bottleneck(nn.Module):
+    expansion = 4
+    def __init__(self,in_channels,out_channels,stride=1,downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels,out_channels,kernel_size=1,stride=1,bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        # -------
+        self.conv2 = nn.Conv2d(out_channels,out_channels,kernel_size=3,stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        # -------
+        self.conv3 = nn.Conv2d(out_channels,out_channels*self.expansion,kernel_size=1,stride=1,bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channels*self.expansion)
+        self.downsample = downsample
+
+    def forward(self, x):
+        identity = x
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        out = self.conv1(x)
+        out = F.relu(self.bn1(out), inplace=True)
+
+        out = self.conv2(out)
+        out = F.relu(self.bn2(out))
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        out += identity
+        out = F.relu(out, inplace=True)
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self,
+                 block,
+                 blocks_num,
+                 num_classes=1000,
+                 include_top=True):
+        super(ResNet, self).__init__()
+        self.include_top = include_top
+        self.in_channel = 64
+
+
+        self.conv1 = nn.Conv2d(3, self.in_channel, kernel_size=7, stride=2,
+                               padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_channel)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, blocks_num[0])
+        self.layer2 = self._make_layer(block, 128, blocks_num[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, blocks_num[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, blocks_num[3], stride=2)
+        if self.include_top:
+            self.avgpool = nn.AdaptiveAvgPool2d((1, 1))  # output size = (1, 1)
+            self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+    def _make_layer(self, block, channel, block_num, stride=1):
+        downsample = None
+        if stride != 1 or self.in_channel != channel * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channel, channel * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(channel * block.expansion))
+
+        layers = []
+        layers.append(block(self.in_channel,
+                            channel,
+                            downsample=downsample,
+                            stride=stride))
+        self.in_channel = channel * block.expansion
+
+        for _ in range(1, block_num):
+            layers.append(block(self.in_channel,
+                                channel))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        if self.include_top:
+            x = self.avgpool(x)
+            x = torch.flatten(x, 1)
+            x = self.fc(x)
+
         return x
 
+
+def resnet34(num_classes=1000, include_top=True):
+    # https://download.pytorch.org/models/resnet34-333f7ec4.pth
+    return ResNet(BasicBlock, [3, 4, 6, 3], num_classes=num_classes, include_top=include_top)
+
+
+def resnet50(num_classes=1000, include_top=True):
+    # https://download.pytorch.org/models/resnet50-19c8e357.pth
+    return ResNet(Bottleneck, [3, 4, 6, 3], num_classes=num_classes, include_top=include_top)
+
+
+def resnet101(num_classes=1000, include_top=True):
+    # https://download.pytorch.org/models/resnet101-5d3b4d8f.pth
+    return ResNet(Bottleneck, [3, 4, 23, 3], num_classes=num_classes, include_top=include_top)
